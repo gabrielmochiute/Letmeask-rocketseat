cfc76b4ece0434b6d7301dfa61327260eb95e89e
gabrielmochiute <gabrielmochiute133@gmail.com>
2021-08-17 11:54:01 -0300

    Inicio da segunda aula de react da NLW

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import { Button } from './components/Button';
+import { createContext, useState } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
+
+export const TestContext = createContext({});
 
 function App() {
+  const [value, setValue] = useState('Teste');
+
   return (
-    <>
-      <Button />
-    </>
+    <BrowserRouter>
+      <TestContext.Provider value={{ value, setValue }}>
+        <Route path="/" component={Home} exact />
+        <Route path="/rooms/new" component={NewRoom} />
+      </TestContext.Provider>
+    </BrowserRouter>
   );
 }
 
